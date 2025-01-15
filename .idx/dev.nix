fde680f7ed5a106306e66df379423d4431bc733d
RokhisAhmadDanii <163649018+RokhisAhmadDanii@users.noreply.github.com>
2025-01-15 16:32:00 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,24 +1,57 @@
-{ pkgs, … }: {
-    packages = [
-pkgs.nodejs-18_x 
-  pkgs.run 
-  pkgs.imagemagick6_light
-  pkgs.mailutils
-  pkgs.python39Packages.pip
-        pkgs.nodePackages.typescript-language-server
-        pkgs.nodePackages.yarn
-        pkgs.replitPackages.jest
-        pkgs.libwebp
-        pkgs.python
-	      pkgs.nodePackages.typescript
-        pkgs.libuuid
-        pkgs.ffmpeg
-        pkgs.imagemagick  
-        pkgs.wget
-        pkgs.git
-        pkgs.nodePackages.pm2
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_18
+    pkgs.neofetch
+    pkgs.jellyfin-ffmpeg
+    pkgs.imagemagick
+    pkgs.libwebp
+    pkgs.bash
+    pkgs.pm2
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
     ];
-  env ={
-    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
   };
 }
